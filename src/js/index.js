a06cc65ae56d429d4e1421f4ff79b5fc7d632c69
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-05-04 15:33:39 +0800

    search model creation

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,23 @@
+import Search from './models/Search';
 // Global app controller
 
-//forkify-api.herokuapp.com
-async function getResults(query) {
+/* Global state of the app:
+- search object
+- current recipe object
+- shopping list object
+- liked recipes */
+const state = {};
 
-}
+const controlSearch = () => {
 
-getResults();
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
+
