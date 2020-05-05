8205e6203d33c06820d8446a29c88c785c0d93b9
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-05-06 00:27:47 +0800

    search result pagination -end

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
@@ -46,11 +47,11 @@ const renderRecipe = recipe => {
 };
 
 const createButton = (page, type) => `
-  <button class="btn-inline results__btn--${type} data-goto=${type === 'prev' ? page - 1 : page + 1}">
+  <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+      <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
       <svg class="search__icon">
           <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
       </svg>
-      <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
   </button>
 `;
 
@@ -65,8 +66,8 @@ const renderButtons = (page, numResults, resPerPage) => {
     } else if (page < pages) {
       // button to past and next page
       button = `
-      createButton(page, 'prev')
-      createButton(page, 'next')
+      ${createButton(page, 'prev')}
+      ${createButton(page, 'next')}
       `;
     } else if (page === pages && pages > 1) {
       // button to past page
